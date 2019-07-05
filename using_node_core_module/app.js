6827b28a7fad3f7ab0b37da041f78419d945a191
abidex4yemi <abidex4yemi@gmail.com>
2019-07-05 18:14:26 +0100

    using node file system module (fs)

--- using_node_core_module/app.js
+++ using_node_core_module/app.js
@@ -7,11 +7,34 @@
 // console.log(pathObj.dir);
 //============================//
 
-const os = require('os');
+/** 
+ * Using os modules
+*/
+// const os = require('os');
 
-const currentUser = os.userInfo();
-const freeMemory = os.freemem();
-const totalMemory = os.totalmem();
-console.log(currentUser);
-console.log(freeMemory);
-console.log(totalMemory);
+// const currentUser = os.userInfo();
+// const freeMemory = os.freemem();
+// const totalMemory = os.totalmem();
+// console.log(currentUser);
+// console.log(freeMemory);
+// console.log(totalMemory);
+//============================//
+
+/** 
+ * Using file system module
+*/
+// const fs = require('fs');
+
+// // Synchronous
+// const file = fs.readdirSync('./');
+// // console.log(file);
+
+// //Asynchronous
+// fs.readdir('./', (err, fileArray) => {
+// 	if (err) {
+// 		console.log(err);
+// 	}
+
+// 	console.log(fileArray);
+// });
+//============================//
