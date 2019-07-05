3dce330557fade22668df10feffd8002239dd40e
abidex4yemi <abidex4yemi@gmail.com>
2019-07-05 17:51:31 +0100

    Using node core modules

--- using_node_core_module/app.js
+++ using_node_core_module/app.js
@@ -6,3 +6,12 @@
 
 // console.log(pathObj.dir);
 //============================//
+
+const os = require('os');
+
+const currentUser = os.userInfo();
+const freeMemory = os.freemem();
+const totalMemory = os.totalmem();
+console.log(currentUser);
+console.log(freeMemory);
+console.log(totalMemory);
